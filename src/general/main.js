5790fd7f67d550c1905daad58159fc2bed52d63c
Henry Minetto <98597098+Henry-Minetto@users.noreply.github.com>
2025-03-25 17:44:01 -0300

    test without document load

--- src/general/main.js
+++ src/general/main.js
@@ -34,7 +34,7 @@ function main() {
 
   //     clearInterval(renderInterval);
   //   }, 1000);
-  
+
   const watchNavigationVtex = () => {
     const targetNode = document.getElementById('corel_container');
     console.log('targetNode', targetNode);
@@ -67,33 +67,34 @@ function main() {
       observer.observe(targetNode, { attributes: true });
     } else {
       console.log('else');
-      const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-            const newTargetNode = document.getElementById('corel_container');
-            if (newTargetNode) {
-              observer.disconnect();
-              sendInteractionsToOrderVtex();
-              if (document?.getElementsByClassName('vtex-product-context-provider')) {
-                mainProductPageView();
-              }
-              fetchProducts().then(promises => {
-                if (promises[0].status === 'fulfilled') {
-                  if (document.querySelector('.shelfWrapper')) {
-                    document.querySelectorAll('.shelfWrapper').forEach((item) => {
-                      item.remove();
-                    });
-                  }
-                  renderShelf(promises[0]?.value);
-                }
-              }).then(() => {
-                initializeSlider();
-                observerHandler();
-              });
+      let lastUrl = location.href;
+      mutations.forEach((mutation) => {
+        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+          const newTargetNode = document.getElementById('corel_container');
+          if (newTargetNode && location.href !== lastUrl) {
+            lastUrl = location.href;
+            observer.disconnect();
+            sendInteractionsToOrderVtex();
+            if (document?.getElementsByClassName('vtex-product-context-provider')) {
+              mainProductPageView();
             }
+            fetchProducts().then(promises => {
+              if (promises[0].status === 'fulfilled') {
+                if (document.querySelector('.shelfWrapper')) {
+                  document.querySelectorAll('.shelfWrapper').forEach((item) => {
+                    item.remove();
+                  });
+                }
+                renderShelf(promises[0]?.value);
+              }
+            }).then(() => {
+              initializeSlider();
+              observerHandler();
+            });
           }
-        });
+        }
       });
+
       observer.observe(document.body, { childList: true, subtree: true });
     }
   };
